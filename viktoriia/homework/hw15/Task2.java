722d9839b3e846d8cbdaa81c6f1a8e878c50078d
Viktoriia Herchanivska <viktoriia.herchanivska@gmail.com>
2023-07-12 21:29:29 +0300

    fix task 2 hw15

--- viktoriia/homework/hw15/Task2.java
+++ viktoriia/homework/hw15/Task2.java
@@ -1,20 +1,41 @@
 package hw15;
 
 public class Task2 {
+    static Object first = new Object();
+    static Object second = new Object();
     public static void main(String[] args) {
         Thread thread1 = new Thread(() -> {
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            synchronized (first) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (second) {
+                    System.out.println("Thread first success");
+                }
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            synchronized (second) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (first) {
+                    System.out.println("Thread second success");
+                }
             }
         });
         thread1.start();
-        Thread.currentThread().stop();
-        try {
-            thread1.join();
+        thread2.start();
+        /*try {
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        System.out.println(thread1.getState());
+        System.out.println(thread2.getState());*/
     }
 }
